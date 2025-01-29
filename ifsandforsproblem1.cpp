0ac0a20423f6784f44b033e74b9848f38fdf3d99
tzuber-dev <zt50481@mail.harpercollege.edu>
2025-01-29 13:40:43 -0600

    Add files via upload

---
+++ ifsandforsproblem1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+
+int main() {
+
+    
+   int integeruserinput;
+    
+
+   
+
+   
+  
+  std::cout << "Enter in a integer number: \n"; 
+  std::cin >> integeruserinput;
+
+ if (integeruserinput % 3 == 0) {
+    printf("Fizz");
+    return 0;
+} else if (integeruserinput % 5 == 0)
+{
+    printf("Buzz");
+
+      return 0;
+}else if (integeruserinput)!5 and !3;
+{
+    printf("FizzBuzz");
+
+    return 0;
+}
+ 
+
+}
+
+
+
+ 
+
+ 
+
+  
+ 
+ 
+
+ 
+    
+
+
+
