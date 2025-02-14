70c83fedbdf009102ecd080b5ef60d48162aa52b
tzuber-dev <zt50481@mail.harpercollege.edu>
2025-02-14 16:24:14 -0600

    Payroll Assignment

---
+++ payrollstub.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main() {
+
+    cout<<"Welcome to the Payroll Program! \n"<<std::endl;
+    
+    
+    double hourlyrate,hoursworked;
+    float numberofchildren;
+    
+
+    // rate logic
+    hourlyrate = 16.78 * 1;
+
+    
+
+    
+
+
+    cout<<"How many hours did you work this week? "<<std::endl;
+    cin>>hoursworked;
+    cout<<"How many children do you have? "<<std::endl;
+    cin>>numberofchildren;
+
+
+    // gross pay logic
+    double gross = hourlyrate *hoursworked;
+
+    // socsec logic
+
+    double socsec = gross * 0.06;
+
+    // fedtax logic
+
+    double fedtax = gross *0.14;
+
+    // sttax logic 
+
+    double sttax = gross *0.05;
+
+    // union logic
+
+    double unionlogic = 10;
+
+    
+
+
+     // insurance logic 
+    
+     double insurancevalue;
+
+     if (numberofchildren >= 3){
+        insurancevalue = 35.00;
+     } else {
+        insurancevalue = 15.00;
+     }; 
+
+     //sum of deductions logic
+     double sumofdeductionslogic = socsec + fedtax + sttax + unionlogic + insurancevalue ;
+    // net logic
+
+     double netlogic = gross - sumofdeductionslogic;
+
+   
+
+    
+ string wagesign = " $/hr";
+    cout<<"Payroll Stub: \n";
+    std::cout<<"*****************************\n"<<std::endl;
+    std::cout<<std::fixed << std::setprecision(1) << "Hours: $ "<<hoursworked<<std::endl;  
+    std::cout<<"-----------------------------\n"<<std::endl;
+    std::cout<<std::fixed << std::setprecision(2) <<"Rate: "<<hourlyrate<<wagesign<<std::endl;
+    std::cout<<"-----------------------------\n"<<std::endl;
+    std::cout<<std::fixed << std::setprecision(2) <<"Gross: $ "<<gross<<std::endl;
+    std::cout<<"-----------------------------\n"<<std::endl;
+    std::cout<<std::fixed << std::setprecision(2)<<"SocSec: $ "<<socsec<<std::endl;
+    std::cout<<"-----------------------------\n"<<std::endl;
+    std::cout<<std::fixed << std::setprecision(2)<<"FedTax: $ "<<fedtax<<std::endl;
+    std::cout<<"-----------------------------\n"<<std::endl;
+    std::cout<<std::fixed << std::setprecision(2)<<"StTax: $ "<<sttax<<std::endl;
+    std::cout<<"-----------------------------\n"<<std::endl;
+    std::cout<<std::fixed << std::setprecision(2)<<"Union: $ "<<unionlogic<<std::endl;
+    std::cout<<"-----------------------------\n"<<std::endl;
+    std::cout<<std::fixed << std::setprecision(2)<<"Ins: $ "<<insurancevalue<<std::endl;
+    std::cout<<"-----------------------------\n"<<std::endl;
+    std::cout<<std::fixed << std::setprecision(2)<<"Net: $ "<<netlogic<<std::endl;
+    std::cout<<"-----------------------------\n"<<std::endl;
+    std::cout<<"Thank you for using the Payroll Program!";
+    
+ return 0;
+
+    
+    
+
+}
